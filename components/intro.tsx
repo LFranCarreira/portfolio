7d6feb8531e36c07f0bd73c884631d87d7c0fa30
Franco Carreira <carreirafranco@gmail.com>
2023-09-12 17:55:20 -0300

    cv updated

--- components/intro.tsx
+++ components/intro.tsx
@@ -91,7 +91,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="https://drive.google.com/file/d/1OGaQiHIXw-C9QiJWucg9f88PcCJyfzi_/view?usp=sharing"
+          href="/CV.pdf"
           download
         >
           Download CV{" "}
